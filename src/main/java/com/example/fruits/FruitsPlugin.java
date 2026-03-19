802ae10a2da05bbc3c369de70c13b15e74aa45ac
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-03-19 22:41:26 +0530

    Update FruitsPlugin.java

--- src/main/java/com/example/fruits/FruitsPlugin.java
+++ src/main/java/com/example/fruits/FruitsPlugin.java
@@ -1,11 +1,24 @@
 package com.example.fruits;
 
+import com.example.fruits.commands.FruitAdminCommand;
+import com.example.fruits.commands.FruitCommand;
+import com.example.fruits.gui.AdminGUIListener;
+import com.example.fruits.listeners.PlayerEatListener;
+import com.example.fruits.models.PlayerFruitData;
+import com.example.fruits.registry.FruitRegistry;
+import com.example.fruits.utils.CooldownManager;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class FruitsPlugin extends JavaPlugin {
     private static FruitsPlugin instance;
     private FruitRegistry fruitRegistry;
     private CooldownManager cooldownManager;
+    private final Map<UUID, PlayerFruitData> activePlayers = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -20,7 +33,7 @@ public class FruitsPlugin extends JavaPlugin {
 
         // Register listeners
         getServer().getPluginManager().registerEvents(new PlayerEatListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
+        getServer().getPluginManager().registerEvents(new AdminGUIListener(), this);
 
         getLogger().info("FruitsPlugin enabled!");
     }
@@ -33,4 +46,5 @@ public class FruitsPlugin extends JavaPlugin {
     public static FruitsPlugin getInstance() { return instance; }
     public FruitRegistry getFruitRegistry() { return fruitRegistry; }
     public CooldownManager getCooldownManager() { return cooldownManager; }
+    public Map<UUID, PlayerFruitData> getActivePlayers() { return activePlayers; }
 }
