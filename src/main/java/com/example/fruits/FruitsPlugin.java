d76f2cd81073093ed001dd245ae6c2d38d66ece3
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-03-19 22:14:31 +0530

    Update FruitsPlugin.java

--- src/main/java/com/example/fruits/FruitsPlugin.java
+++ src/main/java/com/example/fruits/FruitsPlugin.java
@@ -1,1 +1,36 @@
-package com.example.fruits; public class FruitsPlugin {}
+package com.example.fruits;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FruitsPlugin extends JavaPlugin {
+    private static FruitsPlugin instance;
+    private FruitRegistry fruitRegistry;
+    private CooldownManager cooldownManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+        fruitRegistry = new FruitRegistry();
+        cooldownManager = new CooldownManager();
+
+        // Register commands
+        getCommand("fruit").setExecutor(new FruitCommand());
+        getCommand("fruitadmin").setExecutor(new FruitAdminCommand());
+
+        // Register listeners
+        getServer().getPluginManager().registerEvents(new PlayerEatListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
+
+        getLogger().info("FruitsPlugin enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("FruitsPlugin disabled.");
+    }
+
+    public static FruitsPlugin getInstance() { return instance; }
+    public FruitRegistry getFruitRegistry() { return fruitRegistry; }
+    public CooldownManager getCooldownManager() { return cooldownManager; }
+}
